ef902fc498643ed65618a8477a7ea270c6d4fdfc
Pooja Kumari <k.puja14201@gmail.com>
2024-06-06 14:42:34 +0530

    Add test for empty string and initial implementation of add method

---
+++ Calculator/src/main/java/StringCalculator.java
@@ -0,0 +1,8 @@
+public class StringCalculator {
+    public static int add(String numbers) {
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+        return -1;
+    }
+}
---
+++ Calculator/src/test/java/StringCalculatorTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class StringCalculatorTest {
+
+    @Test
+    public void testEmptyString() {
+        assertEquals(0, StringCalculator.add(""));
+    }
+}
